d807526f74fe51e801a9e9ddf06834a5afd2a1ca
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-01-10 17:56:58 +0100

    Update default.js

--- config/default.js
+++ config/default.js
@@ -151,6 +151,7 @@ var config = {
     		    pause: 1300,
             	    payload: 'PUBLISH sip:nodejs@127.0.0.1:5999;transport=udp SIP/2.0\nCall-ID: '+call_id+'\nCSeq: 1 PUBLISH\nFrom: <sip:nodejs@127.0.0.1>;tag=2628881569\nTo: <sip:nodejs@127.0.0.1>;tag=1d24a28a0bded6c40d31e6db8aab9ac6.369f\nVia: SIP/2.0/UDP 192.168.1.1:48495;branch=z9hG4bK9b82aa8fb4c7705466a3456dfff7f384333332\nUser-Agent: HEPGEN-UAS\nContent-Type: application/vq-rtcpxr\nContent-Length: 450\n\nVQIntervalReport\nLocalMetrics:\nSessionDesc:PT=124 PD=opus SR=48000\nTimestamps:START=2015-02-28T21:04:31.000582Z STOP=2015-02-28T21:04:36.000598Z\nCallID:'+call_id+'\nFromID:<sip:caller@domain.net>\nToID:<sip:callee@domain.net>\nOrigID:<sip:caller@domain.net>\nLocalAddr:IP:192.168.1.55 PORT:30539\nLocalMAC:99:e6:ba:df:7b:dd\nRemoteAddr:IP:192.168.1.23 PORT:7079\nRemoteMAC:99:72:b9:28:c2:82\nPacketLoss:NLR=8.0\nDelay:IAJ=93\r\n\r\n'
     	  },
+    	  /*
           {
                 // QoS Report
                   rcinfo: {
@@ -171,6 +172,7 @@ var config = {
     		  pause: 1300,
 		  payload: '{"CORRELATION_ID":"'+call_id+'","RTP_SIP_CALL_ID":"'+call_id+'","DELTA":19.971924,"JITTER":0.162653,"REPORT_TS":1443793820,"TL_BYTE":0,"SKEW":0.701172, "TOTAL_PK":1783,"PACKET_LOSS":0,"SEQ":0,"MAX_JITTER":0.575890,"MAX_DELTA":140.515137,"MAX_SKEW":-1.395996,"MEAN_JITTER":0.196664,"MIN_MOS":4.324768, "MEAN_MOS":4.394112, "MOS":4.394147,"RFACTOR":92.442569,"MIN_RFACTOR":89.429284,"MEAN_RFACTOR":92.440869,"SRC_IP":"192.168.1.1", "SRC_PORT":21122, "DST_IP":"192.168.1.2","DST_PORT":52268,"CODEC_PT":8, "CLOCK":8000,"CODEC_NAME":"pcma","TYPE":"HANGUP"}'
           },
+          */
           {
                 // RTCP Report
                   rcinfo: {
