69138d8e64ae79df2ef4a8adc8f504dc5e09eabb
dletta <jcduschletta@gmail.com>
2024-09-27 15:30:07 -0500

    added mos for second leg

--- config/b2bcall_rtp_multi.js
+++ config/b2bcall_rtp_multi.js
@@ -35,6 +35,7 @@ var isPrivate = function(ip) {
 }
 
 var call_id = Math.random().toString(36).substring(7) + '@127.0.0.1';
+var call_id2 = call_id + '_b2b-1';
 var caller = 'hepgenjs';
 var caller_e164 = '+'+rand(1,99)+'555'+rand(100000,999999);
 var callee = rand(101,199);
@@ -116,7 +117,48 @@ var rtpGenerator = function(config){
                     },
       		  pause: config.pause,
   		  payload: '{"CORRELATION_ID":"'+call_id+'","RTP_SIP_CALL_ID":"'+call_id+'","DELTA":11.983,"JITTER":'+jit2+',"REPORT_TS":'+new Date().getTime()/1000+',"TL_BYTE":0,"SKEW":0.000,"TOTAL_PK":1512,"EXPECTED_PK":1512,"PACKET_LOSS":0,"SEQ":0,"MAX_JITTER":'+jit2+',"MAX_DELTA":20.124,"MAX_SKEW":0.221,"MEAN_JITTER":'+jit2+',"MIN_MOS":'+mos2+', "MEAN_MOS":'+mos2+', "MOS":'+mos2+',"RFACTOR":77.100,"MIN_RFACTOR":77.100,"MEAN_RFACTOR":77.100,"SRC_IP":"'+peer_ip+'", "SRC_PORT":26872, "DST_IP":"'+peer_ip+'","DST_PORT":51354,"SRC_MAC":"00-30-48-7E-5D-C6","DST_MAC":"00-12-80-D7-38-5E","OUT_ORDER":0,"SSRC_CHG":0,"CODEC_PT":9, "CLOCK":8000,"CODEC_NAME":"g722","DIR":0,"REPORT_NAME":"'+peer_ip+':26872","PARTY":0,"TYPE":"PERIODIC"}'
-  	})
+  	},{
+                // RTPAgent RTP media Reports
+                // RTP Report 1 _b2b-1
+                  rcinfo: {
+                          type: 'HEP',
+                          version: 3,
+                          payload_type: 'JSON',
+                          captureId: 2001,
+                          capturePass: capturePass,
+                          ip_family: 2,
+                          protocol: 17,
+                          proto_type: 34,
+                          srcIp: pub_ip,
+                          dstIp: peer_ip,
+                          srcPort: rtpports.src || 7000,
+                          dstPort: rtpports.dst || 10000,
+                          mos: parseInt(mos1 * 100),
+                          correlation_id: call_id2
+                  },
+                      pause: 500,
+                  payload: '{"CORRELATION_ID":"'+call_id2+'","RTP_SIP_CALL_ID":"'+call_id2+'","DELTA":19.983,"JITTER":'+jit1+',"REPORT_TS":'+new Date().getTime()/1000+',"TL_BYTE":0,"SKEW":0.000,"TOTAL_PK":1512,"EXPECTED_PK":1512,"PACKET_LOSS":0,"SEQ":0,"MAX_JITTER":'+jit1+',"MAX_DELTA":20.024,"MAX_SKEW":0.172,"MEAN_JITTER":'+jit1+',"MIN_MOS": '+mos1+', "MEAN_MOS":'+mos1+', "MOS":'+mos1+',"RFACTOR":80.200,"MIN_RFACTOR":80.200,"MEAN_RFACTOR":80.200,"SRC_IP":"'+pub_ip+'", "SRC_PORT":26872, "DST_IP":"'+peer_ip+'","DST_PORT":51354,"SRC_MAC":"00-30-48-7E-5D-C6","DST_MAC":"00-12-80-D7-38-5E","OUT_ORDER":0,"SSRC_CHG":0,"CODEC_PT":9, "CLOCK":8000,"CODEC_NAME":"g722","DIR":0,"REPORT_NAME":"'+pub_ip+':26872","PARTY":0,"TYPE":"PERIODIC"}'
+        }, {
+                // RTP Report 2 _b2b-1
+                  rcinfo: {
+                          type: 'HEP',
+                          version: 3,
+                          payload_type: 'JSON',
+                          captureId: 2001,
+                          capturePass: capturePass,
+                          ip_family: 2,
+                          protocol: 17,
+                          proto_type: 34,
+                          srcIp: peer_ip,
+                          dstIp: pub_ip,
+                          dstPort: rtpports.src || 7000,
+                          srcPort: rtpports.dst || 10000,
+                          mos: parseInt(mos2 * 100),
+                          correlation_id: call_id2
+                  },
+                      pause: config.pause,
+                  payload: '{"CORRELATION_ID":"'+call_id2+'","RTP_SIP_CALL_ID":"'+call_id2+'","DELTA":11.983,"JITTER":'+jit2+',"REPORT_TS":'+new Date().getTime()/1000+',"TL_BYTE":0,"SKEW":0.000,"TOTAL_PK":1512,"EXPECTED_PK":1512,"PACKET_LOSS":0,"SEQ":0,"MAX_JITTER":'+jit2+',"MAX_DELTA":20.124,"MAX_SKEW":0.221,"MEAN_JITTER":'+jit2+',"MIN_MOS":'+mos2+', "MEAN_MOS":'+mos2+', "MOS":'+mos2+',"RFACTOR":77.100,"MIN_RFACTOR":77.100,"MEAN_RFACTOR":77.100,"SRC_IP":"'+peer_ip+'", "SRC_PORT":26872, "DST_IP":"'+peer_ip+'","DST_PORT":51354,"SRC_MAC":"00-30-48-7E-5D-C6","DST_MAC":"00-12-80-D7-38-5E","OUT_ORDER":0,"SSRC_CHG":0,"CODEC_PT":9, "CLOCK":8000,"CODEC_NAME":"g722","DIR":0,"REPORT_NAME":"'+peer_ip+':26872","PARTY":0,"TYPE":"PERIODIC"}'
+        })
   }
 
   reports.push({
@@ -196,12 +238,94 @@ var rtpGenerator = function(config){
                             dstIp: peer_ip,
                             srcPort: rtpports.src || 7000,
                             dstPort: rtpports.dst || 10000,
-                            correlation_id: call_id+'_b2b-1',
+                            correlation_id: call_id,
   			    mos: parseInt(finalStats.mos2*100)
                     },
       		  pause: 50,
   		  payload: '{"CORRELATION_ID":"'+call_id+'", "RTP_SIP_CALL_ID":"'+call_id+'","MOS":'+finalStats.mos2+',"RFACTOR":91.200,"DIR":1,"REPORT_NAME":"'+pub_ip+'","PARTY":1,"TYPE":"FINAL"}'
-  	})
+  	},{
+                // RTP Report FINAL A-leg IN _b2b-1
+                  rcinfo: {
+                          type: 'HEP',
+                          version: 3,
+                          payload_type: 'JSON',
+                          captureId: 2001,
+                          capturePass: capturePass,
+                          ip_family: 2,
+                          protocol: 17,
+                          proto_type: 34,
+                          srcIp: peer_ip,
+                          dstIp: pub_ip,
+                          srcPort: rtpports.src || 7000,
+                          dstPort: rtpports.dst || 10000,
+                            mos: parseInt(finalStats.mos2*100), // 440
+                          correlation_id: call_id2
+                  },
+                      pause: 2500,
+                  payload: '{"CORRELATION_ID":"'+call_id2+'","RTP_SIP_CALL_ID":"'+call_id2+'","DELTA":19.983,"JITTER":0.017,"REPORT_TS":'+new Date().getTime()/1000+',"TL_BYTE":241833,"SKEW":0.000,"TOTAL_PK":1511,"EXPECTED_PK":1512,"PACKET_LOSS":1,"SEQ":0,"MAX_JITTER":0.010,"MAX_DELTA":20.024,"MAX_SKEW":0.172,"MEAN_JITTER":0.005,"MIN_MOS":'+finalStats.mos2+', "MEAN_MOS":'+finalStats.mos2+', "MOS":'+finalStats.mos2+',"RFACTOR":80.200,"MIN_RFACTOR":80.200,"MEAN_RFACTOR":80.200,"SRC_IP":"'+pub_ip+'", "SRC_PORT":26872, "DST_IP":"'+peer_ip+'","DST_PORT":51354,"SRC_MAC":"00-30-48-7E-5D-C6","DST_MAC":"00-12-80-D7-38-5E","OUT_ORDER":0,"SSRC_CHG":0,"CODEC_PT":9, "CLOCK":8000,"CODEC_NAME":"g722","DIR":0,"REPORT_NAME":"'+pub_ip+':26872","PARTY":0,"TYPE":"FINAL"}'
+        }, {
+
+                // RTP Report FINAL A-leg OUT _b2b-1
+                  rcinfo: {
+                          type: 'HEP',
+                          version: 3,
+                          payload_type: 'JSON',
+                          captureId: 2001,
+                          capturePass: capturePass,
+                          ip_family: 2,
+                          protocol: 17,
+                          proto_type: 34,
+                          srcIp: pub_ip,
+                          dstIp: peer_ip,
+                          srcPort: rtpports.src || 7000,
+                          dstPort: rtpports.dst || 10000,
+                            mos: parseInt(finalStats.mos1*100), // 440
+                          correlation_id: call_id2
+                  },
+                      pause: 500,
+                  payload: '{"CORRELATION_ID":"'+call_id2+'","RTP_SIP_CALL_ID":"'+call_id2+'","DELTA":19.983,"JITTER":0.027,"REPORT_TS":'+new Date().getTime()/1000+',"TL_BYTE":241920,"SKEW":0.000,"TOTAL_PK":1512,"EXPECTED_PK":1512,"PACKET_LOSS":0,"SEQ":0,"MAX_JITTER":0.020,"MAX_DELTA":20.024,"MAX_SKEW":0.172,"MEAN_JITTER":0.020,"MIN_MOS": '+finalStats.mos1+', "MEAN_MOS":'+finalStats.mos1+', "MOS":'+finalStats.mos1+',"RFACTOR":80.200,"MIN_RFACTOR":80.200,"MEAN_RFACTOR":80.200,"SRC_IP":"'+pub_ip+'", "SRC_PORT":26872, "DST_IP":"'+peer_ip+'","DST_PORT":51354,"SRC_MAC":"00-30-48-7E-5D-C6","DST_MAC":"00-12-80-D7-38-5E","OUT_ORDER":0,"SSRC_CHG":0,"CODEC_PT":9, "CLOCK":8000,"CODEC_NAME":"g722","DIR":1,"REPORT_NAME":"'+peer_ip+':26872","PARTY":1,"TYPE":"FINAL"}'
+
+        }, {
+                // RTP Final Report 1 _b2b-1
+                  rcinfo: {
+                          type: 'HEP',
+                          version: 3,
+                          payload_type: 'JSON',
+                          captureId: 2001,
+                          capturePass: capturePass,
+                          ip_family: 2,
+                          protocol: 17,
+                          proto_type: 35,
+                          srcIp: peer_ip,
+                          dstIp: pub_ip,
+                          srcPort: rtpports.src || 7000,
+                          dstPort: rtpports.dst || 10000,
+                          correlation_id: call_id2,
+                            mos: parseInt(finalStats.mos1*100), // 440
+                  },
+                      pause: 50,
+                  payload: '{"CORRELATION_ID":"'+call_id2+'", "RTP_SIP_CALL_ID":"'+call_id2+'","MOS":'+finalStats.mos1+',"RFACTOR":93.200,"DIR":0,"REPORT_NAME":"'+peer_ip+'","PARTY":0,"TYPE":"FINAL"}'
+        }, {
+                // RTP Final Report 2 _b2b-1
+                  rcinfo: {
+                          type: 'HEP',
+                          version: 3,
+                          payload_type: 'JSON',
+                          captureId: 2001,
+                          capturePass: capturePass,
+                          ip_family: 2,
+                          protocol: 17,
+                          proto_type: 35,
+                          srcIp: pub_ip,
+                          dstIp: peer_ip,
+                          srcPort: rtpports.src || 7000,
+                          dstPort: rtpports.dst || 10000,
+                          correlation_id: call_id2,
+                            mos: parseInt(finalStats.mos2*100)
+                  },
+                      pause: 50,
+                  payload: '{"CORRELATION_ID":"'+call_id2+'", "RTP_SIP_CALL_ID":"'+call_id2+'","MOS":'+finalStats.mos2+',"RFACTOR":91.200,"DIR":1,"REPORT_NAME":"'+pub_ip+'","PARTY":1,"TYPE":"FINAL"}'
+        })
 
   return reports;
 };
