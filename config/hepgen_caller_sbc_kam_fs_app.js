ed28e64e7c0e8dab60984da7a81f5f410b1ac8bf
Jachen Duschletta <jcduschletta@gmail.com>
2025-06-24 11:55:08 -0500

    Update hepgen_caller_sbc_kam_fs_app.js
    DTMF time

--- config/hepgen_caller_sbc_kam_fs_app.js
+++ config/hepgen_caller_sbc_kam_fs_app.js
@@ -1125,11 +1125,11 @@ var config = {
                                 srcIp: '192.168.1.10', dstIp: '192.168.1.11',
                                 srcPort: rtpPorts.fs_app.app_rtp, dstPort: rtpPorts.fs_app.fs_rtp,
                                 mos: 436,
-				time_sec: Math.floor(Date.now() / 1000),
+				time_sec: Math.floor(Date.now() / 1000) + 1,
                                 time_usec: Math.floor((Date.now() % 1000) * 1000)
                   	},
                   	pause: 1000, // Longer pause before media starts
-                  	payload: '{"CORRELATION_ID":"' + a_leg_call_id + '",' + '"REPORT_TS":' + Date.now() / 1000 + ',' + '"DTMF":"ts:' + Date.now() / 1000 + ',tsu:843750,e:1,v:15,d:160,c:1"}'
+                  	payload: '{"CORRELATION_ID":"' + a_leg_call_id + '",' + '"REPORT_TS":' + Math.floor(Date.now() / 1000) + ',' + '"DTMF":"ts:' + Date.now() / 1000 + ',tsu:843750,e:1,v:15,d:160,c:1"}'
                 },
 
 		// === MEDIA PHASE: Insert RTP reports here ===
